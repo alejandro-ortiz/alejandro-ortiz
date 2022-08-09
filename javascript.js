bda1630b2f9665ec4ad6746d854d6b238076443b
Alex Ortiz <alex@ortizaggies.com>
2022-08-08 20:45:12 -0500

    removed the click away menu thing
    it didn't even work on mobile and was causing trouble so whatever

--- javascript.js
+++ javascript.js
@@ -3,12 +3,12 @@ function dropDownFunction() {
     document.getElementById("dropdownbutton").classList.toggle("change");
 }
 
-window.onclick = function (event) {
-    if (!event.target.matches('#dropdownbutton')) {
-        const dropdowns = document.getElementById("myDropdown");
-        if (dropdowns.classList.contains('show')) {
-            dropdowns.classList.remove('show');
-            document.getElementById("dropdownbutton").classList.toggle("change");
-        }
-    }
-}
+// window.onclick = function (event) {
+//     if (!event.target.matches('#dropdownbutton')) {
+//         const dropdowns = document.getElementById("myDropdown");
+//         if (dropdowns.classList.contains('show')) {
+//             dropdowns.classList.remove('show');
+//             document.getElementById("dropdownbutton").classList.toggle("change");
+//         }
+//     }
+// }
